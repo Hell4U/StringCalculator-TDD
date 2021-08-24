0523bbfa89aa1ddf110060199b8bfec4c94f7385
Hell4U <18BCE131@nirmauni.ac.in>
2021-08-24 19:53:30 +0530

    2. Second Test single value string (GREEN)

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
     public int add(String string){
+        if(string.isEmpty()){
         return 0;
+        } else{
+            return Integer.parseInt(string);
+        }
     }
 }
