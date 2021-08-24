19ab355dd3f25aa4a7156fca50b3be33ea210abe
Hell4U <18BCE131@nirmauni.ac.in>
2021-08-24 19:45:10 +0530

    1. First Test empty string (RED)

---
+++ src/test/java/CalculatorTester.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class CalculatorTester {
+    Calculator calculator ;
+
+    @Before
+    public void init(){
+        calculator=new Calculator();
+    }
+
+    @Test
+    public void emptyStringTest(){
+        Assert.assertEquals(0,calculator.add(""));
+    }
+}
