e496be87b9938000da4ed0a4204864eb622321eb
Hell4U <18BCE131@nirmauni.ac.in>
2021-08-24 20:27:54 +0530

    3. Third Test two value string (GREEN)

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,13 @@
 public class Calculator {
     public int add(String input){
+
         if(empty(input)){
-        return 0;
-        } else{
+            return 0;
+        } else if(input.length()==1){
             return stringToInt(input);
+        } else{
+            String[] nums=input.split(",");
+            return Integer.parseInt(nums[0])+Integer.parseInt(nums[1]);
         }
     }
 
